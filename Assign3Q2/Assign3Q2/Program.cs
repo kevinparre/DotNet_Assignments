39d817d72f1285761a0b0fcdc615eb66758fef3a
Administrator <Administrator@WINDOWS-UNR7VLH>
2022-07-26 01:27:38 +0530

    Add project files.

---
+++ Assign3Q2/Assign3Q2/Program.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assign3Q2
+{
+    abstract class Student
+    {
+        protected string name;
+        protected int id;
+       protected Double grade;
+
+       
+
+
+        abstract public bool Ispassed(Double grade);
+    }
+    class Undergraduate : Student
+
+   
+    {
+
+        public Undergraduate(string name, int id, double grade)
+        {
+            this.name = name;
+            this.id = id;
+            this.grade = grade;
+
+        }
+        public override bool Ispassed(Double grade)
+        {
+            if (grade > 70)
+            {
+                return true;
+
+            }
+            else
+                return false;
+            
+        }
+    }
+
+    class Graduate : Student
+    {
+        public Graduate(string name, int id, double grade)
+        {
+            this.name = name;
+            this.id = id;
+            this.grade = grade;
+
+        }
+        public override bool Ispassed(Double grade)
+        {
+            if (grade > 80)
+            {
+                
+                return true;
+
+            }
+            else
+                return false;
+            
+        }
+    }
+        class Program
+    {
+        static void Main(string[] args)
+        {
+            string name;
+            int id;
+            double grade;
+            string type;
+
+            Console.WriteLine("Enter the Name:");
+            name = Console.ReadLine();
+            Console.WriteLine("Enter the ID:");
+            id =Convert.ToInt32( Console.ReadLine());
+            Console.WriteLine("Enter the Grade:");
+            grade =Convert.ToDouble( Console.ReadLine());
+
+            Console.WriteLine("Enter the Class(Graduate/Undergraduate):");
+            type = Convert.ToString(Console.ReadLine());
+            
+            if (type == "g")
+            {
+                Student student = new Graduate(name, id, grade);
+                if (student.Ispassed(grade))
+                {
+                    Console.WriteLine("You Have Passed !!!");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry! You have Failed.");
+
+                }
+            }
+            else
+            { 
+                Student student2 = new Undergraduate(name, id, grade);
+                if (Convert.ToBoolean(student2.Ispassed(grade)) == true)
+                {
+                    Console.WriteLine("You Have Passed !!!");
+                }
+                else
+                {
+                    Console.WriteLine("Sorry! You have Failed.");
+
+                }
+            }
+
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ Assignment3q1/Assignment3q1/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment3q1
+{
+     public class Employee
+    {
+        int empid;
+        string empname;
+        string address;
+        string city;
+        string dept;
+        double salary=10000;
+
+        public virtual double GetSalary()
+        {
+            return salary;
+
+        }
+
+    }
+
+
+
+    class ContractEmployee : Employee
+    {
+        double perks;
+        public override double GetSalary()
+        {
+            Console.WriteLine("Enter Perks:");
+            perks = Convert.ToDouble(Console.ReadLine());
+            return base.GetSalary() + perks;
+        }
+
+    }
+    class PermanentEmployee : Employee
+    {
+        int leaves;
+        double PFfund;
+
+        public override double GetSalary()
+        {
+            Console.WriteLine("Enter Provident Fund:");
+            PFfund = Convert.ToDouble(Console.ReadLine()); 
+
+            return base.GetSalary() - PFfund;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int id;
+            string name;
+            string address;
+            string city;
+            string dept;
+            int type;
+            double finalsal;
+
+            Employee emp = new Employee();
+
+            Console.WriteLine("Console based Application");
+            Console.WriteLine("Salary Calculation ");
+            Console.WriteLine("Enter you Employee's Name:");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee's ID");
+            id = Convert.ToInt32( Console.ReadLine());
+
+            Console.WriteLine("Enter Employee's Address:");
+            address = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee's City:");
+            city= Console.ReadLine();
+
+            Console.WriteLine("Enter Employee's Department:");
+            dept = Console.ReadLine();
+
+            Console.WriteLine("Enter Employee Type:1)Contract Employee  2)Permanent Employee:");
+            type =Convert.ToInt32( Console.ReadLine());
+
+            if(type == 1)
+            {
+                emp = new ContractEmployee();
+                finalsal= emp.GetSalary();
+                Console.WriteLine("Total Salary :{0}",finalsal);
+            }
+            else
+            {
+                emp = new PermanentEmployee();
+                finalsal = emp.GetSalary();
+                Console.WriteLine("Total Salary :{0}", finalsal);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
